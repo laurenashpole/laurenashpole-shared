b16759645217175f80519b1606e4810037cbc123
laurenashpole <lauren@laurenashpole.com>
2021-07-01 00:42:30 -0400

    Created header stub

---
+++ components/header/Header.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const Header = ({ links }) => {
+  return (
+    <header>Header</header>
+  );
+};
+
+Home.propTypes = {
+  links: PropTypes.array
+};
+
+export default Header;
