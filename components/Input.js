19141c75c8796428a3157dc9e4097066f9953a08
laurenashpole <lauren@laurenashpole.com>
2025-02-18 23:09:10 -0500

    Use Vercel blob for uploads

--- components/Input.js
+++ components/Input.js
@@ -1,18 +1,19 @@
+import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/Input.styles.js';
 
-const Input = ({ label, hideLabel, hasError, attributes }) => {
+const Input = forwardRef(({ label, hideLabel, hasError, attributes }, ref) => {
   return (
     <div className={`input ${hasError ? 'input--error' : ''}`}>
       <label className={`input__label ${hideLabel ? 'input__label--hidden' : ''}`} htmlFor={label.replace(/ /g, '')} dangerouslySetInnerHTML={{ __html: label }} />
-      <input className="input__input" id={label.replace(/ /g, '')} aria-invalid={hasError ? true : null} aria-describedby={hasError ? `${label.replace(/ /g, '')}Error` : null} {...attributes} />
+      <input className="input__input" id={label.replace(/ /g, '')} aria-invalid={hasError ? true : null} aria-describedby={hasError ? `${label.replace(/ /g, '')}Error` : null} {...attributes} ref={ref} />
 
       <style jsx global>
         {styles}
       </style>
     </div>
   );
-};
+});
 
 Input.propTypes = {
   label: PropTypes.string,
@@ -20,5 +21,7 @@ Input.propTypes = {
   hasError: PropTypes.bool,
   attributes: PropTypes.object
 };
+
+Input.displayName = 'Input';
 
 export default Input;
